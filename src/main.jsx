605e0fff92a83477f4882effc28da0569e1ba8ae
moen-roy <moenr49@gmail.com>
2025-10-23 22:53:22 +0300

    adding images and caring section

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './components/index.css'
-
+import CaringSection from './components/section/CaringSection'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <CaringSection />
   </StrictMode>,
 )
